c61de6a677722669c4ca97a56224e281b3f27811
jonjoshelvey0 <oliver.stachowiak@student.adelaide.edu.au>
2023-09-27 15:09:53 +0930

    1

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+    for (int a = 0; a < 10; ++a)
+        if (a % 2 == 0)
+            cout << a << ' ';
+    
+
+
+
+
+    return 0;
+}
