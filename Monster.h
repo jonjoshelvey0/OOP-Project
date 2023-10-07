a93f6961f560eb258a0e4c6ddab2e0d1f4ec71fb
jonjoshelvey0 <oliver.stachowiak@student.adelaide.edu.au>
2023-10-07 23:26:31 +1030

    1

--- Monster.h
+++ Monster.h
@@ -7,38 +7,18 @@ using namespace std;
 
 class Monster : public Npc
 {
-    string type;
     int damage;
 
 public:
-    Monster()
+    Monster(string name, string type) : Npc{name, type} 
     {
-
+        
     }
 
     void onceDefeated()
     {
 
     }
-
-    void giveQuestion(Player& abc)
-    {
-
-        // if (type == "grammar")
-        // {
-
-        // }
-        // else if ()
-
-        // verifyInput();
-
-        // abc.getHealth();
-        
-
-        // if (wrong)
-        //     attackPlayer();
-    }
-
 };
 
 #endif
