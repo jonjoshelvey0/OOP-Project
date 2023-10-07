304ffd24d28ba01254388d0dd1bb1680058fcc3e
jonjoshelvey0 <oliver.stachowiak@student.adelaide.edu.au>
2023-10-07 23:01:22 +1030

    1

--- playGame.h
+++ playGame.h
@@ -18,8 +18,30 @@
 
 using namespace std;
 
-Npc* getNPC();
-void endGameAssesment(Player& player);
+Npc* getNPC()
+{
+    if (Random::get(1, 10) == 1) // return an elf instead of a monster, 1 in 10 chance
+        return new Elf{};
+    
+    switch (Random::get(1, 4))
+    {
+        case 1:
+            return new ScienceSphynx{};
+        case 2:
+            return new MathsMinotaur{};
+        case 3:
+            return new GeographyGiant{};
+        case 4:
+            return new GrammarGoblin{};
+    }
+
+    return NULL;
+}
+
+void endGameAssesment(Player& player)
+{
+
+}
 
 void saveGame(Player& player)
 {
