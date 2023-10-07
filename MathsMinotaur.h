02bd874a5724559766c52279a25cb19372d244e6
Oggy Stankovic <oggystankovic@Oggys-MacBook-Pro.local>
2023-10-07 19:57:45 +1030

    e

--- MathsMinotaur.h
+++ MathsMinotaur.h
@@ -6,69 +6,102 @@
 
 using namespace std;
 
-class MathsMinotaur: public Monster
+class MathsMinotaur : public Monster
 {
 
 public:
+    void giveMultTable() // Random generator of a multiplication quesiton
+    {
 
+        int x = Random::get(1, 12);
+        int y = Random::get(1, 12);
 
-void giveMultTable()
-{
-   
-    int x = Random::get(1, 12);
-    int y = Random::get(1, 12);
+        int correctAnswer = x * y;
 
-    
-    int correctAnswer = x * y;
+        cout << "What is " << x << " * " << y << "? ";
 
+        int userAnswer;
+        cin >> userAnswer;
 
-    cout << "What is " << x << " * " << y << "? ";
-
-    
-    int userAnswer;
-    cin >> userAnswer;
-
-    
-    if (userAnswer == correctAnswer) {
-        cout << "CORRECT !! " << endl;
-    } else {
-        cout << " INCORRECT !! " << correctAnswer << "." << endl;
+        if (userAnswer == correctAnswer)
+        {
+            cout << "CORRECT !! " << endl;
+        }
+        else
+        {
+            cout << " INCORRECT, the correct answer is !! " << correctAnswer << "." << endl;
+        }
     }
-}
 
-void giveDivisonQuestion()
-{
-    int dividend = Random::get(1, 100);
-    int divisor =   Random::get(1, 10); 
-  
-    dividend = dividend - (dividend % divisor);
-    
-    int correctAnswer = dividend / divisor;
-    cout << "What is " << dividend << " divided by " << divisor << "? ";
+    void giveDivisonQuestion() // random generator of a division quesiton
+    {
+        int dividend = Random::get(1, 100);
+        int divisor = Random::get(1, 10);
 
-    int userAnswer;
-    cin >> userAnswer;
+        dividend = dividend - (dividend % divisor);
 
-    if (userAnswer == correctAnswer) {
-        cout << "CORRECT !! " << endl;
-    } else {
-        cout << "INCORRECT !! The correct answer is " << correctAnswer << "." << endl;
+        int correctAnswer = dividend / divisor;
+        cout << "What is " << dividend << " divided by " << divisor << "? ";
+
+        int userAnswer;
+        cin >> userAnswer;
+
+        if (userAnswer == correctAnswer)
+        {
+            cout << "CORRECT !! " << endl;
+        }
+        else
+        {
+            cout << "INCORRECT !! The correct answer is " << correctAnswer << "." << endl;
+        }
     }
-}
 
+    void additionQuesotion()
+    {
 
+        int x = Random::get(1, 100);
+        int y = Random::get(1, 100);
 
+        int answerC = x + y;
 
+        cout << "What is " << x << " divided by " << y << "? ";
 
+        int userAnswer;
+        cin >> userAnswer;
+
+        if (userAnswer == answerC)
+        {
+            cout << "CORRECT !! " << endl;
+        }
+        else
+        {
+            cout << "INCORRECT !! The correct answer is " << answerC << "." << endl;
+        }
+    }
+
+    void subtractionQuesotion()
+    {
+
+        int x = Random::get(1, 100);
+        int y = Random::get(1, 100);
+
+        int answerC = x - y;
+
+        cout << "What is " << x << " subtracted by " << y << "? ";
+
+        int userAnswer;
+        cin >> userAnswer;
+
+        if (userAnswer == answerC)
+        {
+            cout << "CORRECT !! " << endl;
+        }
+        else
+        {
+            cout << "INCORRECT !! The correct answer is " << answerC << "." << endl;
+        }
+    }
 };
-   
-
-
-
-
-
-
-
-
 
 #endif
+
